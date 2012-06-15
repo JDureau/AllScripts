f9365cc26306adf4091e0db20cc7c5357c071a23
Joseph Dureau <dureau.joseph@gmail.com>
2012-06-15 12:14:37 +0200

    plenty of SEIR

--- SpecificToCluster/MarcNewModels.m
+++ SpecificToCluster/MarcNewModels.m
@@ -13,9 +13,11 @@ addpath([pwd '/Epidemic Models/Disease-specific code/SEIR'])
 
 SavePath = '/users/ecologie/dureau/src/AllData/ResultsMarc/';
 addpath([pwd '/Epidemic Models/Disease-specific code/SEIR2_1diff'])
+addpath([pwd '/Epidemic Models/Disease-specific code/SEIR2_1diffb'])
 addpath([pwd '/Epidemic Models/Disease-specific code/SEIR2_2diff'])
 addpath([pwd '/Epidemic Models/Disease-specific code/SEIR2_2diffb'])
 addpath([pwd '/Epidemic Models/Disease-specific code/SEIR2_3diff'])
+addpath([pwd '/Epidemic Models/Disease-specific code/SEIR2_4diff'])
 
 
 
