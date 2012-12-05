126fe73661b1e32bf2fe5f9e6503a3b014212ee3
JDureau <dureau.joseph@gmail.com>
2012-12-05 14:05:46 +0100

    more districts Avahan determ

--- SpecificToCluster/HIVRealData_Determ.m
+++ SpecificToCluster/HIVRealData_Determ.m
@@ -137,11 +137,11 @@ elseif ind == 9
     temp = load([SavePath 'ParametersChennai' mode '.mat']); 
     
     Parameters = temp.Parameters;
-    ObsYearsChennai = [2006.588	2006.9 2009.5 2009.6];
-    ObsChennai = [0.0317 0.022 0.02 0.12];
-    ObsChennaiMin = [0.008 0.0085 0.005 0.073];
-    ObsChennaiMax = [0.055 0.036 0.035 0.166];
-    ObsVarsChennai = [7 8 7 8];
+    ObsYears = [2006.588	2006.9 2009.5 2009.6];
+    Obs = [0.0317 0.022 0.02 0.12];
+    ObsMin = [0.008 0.0085 0.005 0.073];
+    ObsMax = [0.055 0.036 0.035 0.166];
+    ObsVars = [7 8 7 8];
     NbSamples = [410 405 397 408];
 
 
@@ -151,11 +151,11 @@ elseif ind == 10
     temp = load([SavePath 'ParametersMadurai' mode '.mat']); 
     
     Parameters = temp.Parameters;
-    ObsYearsMadurai = [2006.3 2006.9	2009.3  2009.67];
-    ObsMadurai = [0.057	 0.0225 0.088 0.037 ];
-    ObsMaduraiMin = [0.035 0.005 0.058 0.0146];
-    ObsMaduraiMax = [0.079 0.04 0.118 0.0617];
-    ObsVarsMadurai = [7 8 7 8];
+    ObsYears = [2006.3 2006.9	2009.3  2009.67];
+    Obs = [0.057	 0.0225 0.088 0.037 ];
+    ObsMin = [0.035 0.005 0.058 0.0146];
+    ObsMax = [0.079 0.04 0.118 0.0617];
+    ObsVars = [7 8 7 8];
     NbSamples = [402 400 396 401];
 
     Name = ['/HIV_Shimoga' s '.mat'];
@@ -164,11 +164,11 @@ elseif ind == 11
     temp = load([SavePath 'ParametersSalem' mode '.mat']); 
     
     Parameters = temp.Parameters;
-    ObsYearsSalem = [2006.3	2006.8 2009.3  2009.47];
-    ObsSalem = [0.1294 	 0.035 0.113 0.019];
-    ObsSalemMin = [0.0922 0.011 0.064 0.00];
-    ObsSalemMax = [0.1665 0.058 0.1617 0.041];
-    ObsVarsSalem = [7 8 7 8];
+    ObsYears = [2006.3	2006.8 2009.3  2009.47];
+    Obs = [0.1294 	 0.035 0.113 0.019];
+    ObsMin = [0.0922 0.011 0.064 0.00];
+    ObsMax = [0.1665 0.058 0.1617 0.041];
+    ObsVars = [7 8 7 8];
     NbSamples = [392 396 407 407];
 
     Name = ['/HIV_Shimoga' s '.mat'];
