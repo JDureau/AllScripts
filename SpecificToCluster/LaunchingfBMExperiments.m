c66baf91202416a2af5706c70a5cf3c0e80037b6
JDureau <dureau.joseph@gmail.com>
2013-01-21 16:14:20 +0100

    fBM for cluster

--- SpecificToCluster/LaunchingfBMExperiments.m
+++ SpecificToCluster/LaunchingfBMExperiments.m
@@ -32,6 +32,8 @@ Par.mu_X.Value = 0.1;
 Par.X0.Value = 0.8;
 Par.kappa.Value = 0.027;
 
+Par.Names.All = {'H','sigma_X','mu_Y','rho','kappa','mu_X','X0'};
+
 Par.H.MinLim = 0;
 Par.H.MaxLim = 1;
 Par.H.Transf = @logit;
